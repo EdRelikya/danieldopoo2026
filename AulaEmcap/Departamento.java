0eff7faa5ecb6e9e40e7e83266c9357be1180ff6
Ed <edmundo.00000859930@unicap.br>
2026-08-21 23:08:56 +0000

    Att aula emcap

---
+++ AulaEmcap/Departamento.java
@@ -0,0 +1,31 @@
+package AulaEmcap;
+
+public class Departamento {
+    private int codigo;
+    private String nome;
+
+
+    Departamento(int codigo, String nome){
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    
+}
---
+++ AulaEmcap/MainEmcap.java
@@ -0,0 +1,17 @@
+package AulaEmcap;
+
+public class MainEmcap {
+
+ public static void main(String[] args) {
+
+
+    Departamento derpartamento = new Departamento(123, "Ed");
+
+    System.out.println(derpartamento.getNome());
+
+
+
+    
+ }
+    
+}
